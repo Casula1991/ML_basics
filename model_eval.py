a1f54be46989c601ec12b4a5631878dcae2dbf43
Fabio <fabiosales_2l@hotmail.com>
2023-04-29 19:25:15 -0300

    tudo organizado

--- model_eval.py
+++ model_eval.py
@@ -20,8 +20,6 @@ p3 = pickle.load(open(path__ + "poly3fit.sav",'rb'))
 pol2 = PolynomialFeatures(degree = 2)
 pol3 = PolynomialFeatures(degree = 3)
 
-#print(lm, p2, p3)
-
 def rmse(ytrue, ypred):
 	return np.sqrt(((ytrue-ypred)**2).mean())
 
@@ -35,14 +33,11 @@ for b in range(B):
 	lmpred = lm.predict(X)
 	p2pred = p2.predict(pol2.fit_transform(X))
 	p3pred = p3.predict(pol3.fit_transform(X))
-
 	r1 = rmse(ytrue = y, ypred = lmpred)
 	r2 = rmse(ytrue = y, ypred = p2pred)
 	r3 = rmse(ytrue = y, ypred = p3pred)
-
 	if save_it == 'sim':
 		L1.append(r1); L2.append(r2); L3.append(r3)
-
 	print('lin:', r1)
 	print('pol2:', r2)
 	print('pol3:', r3)
@@ -54,18 +49,13 @@ print(dfres.head())
 print()
 print('Avg error:')
 print(dfres.mean(axis = 0))
-
-
-
 if final_plot == 'sim':
-
 	plt.figure(figsize = [10,6])
 	plt.hist(L1, bins = 50, label = 'lin_reg')
 	plt.hist(L2, bins = 50, label = 'poly2')
 	plt.hist(L3, bins = 50, label = 'poly3')
 	plt.legend(fontsize = 14)
 	plt.grid()
-
 plt.show()
 
 
