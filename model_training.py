e9a42d007e67deddad2efe8674f1dd081365629c
Fabio <fabiosales_2l@hotmail.com>
2023-04-29 17:51:20 -0300

    criando os modelos lin/pol2 e 3

--- model_training.py
+++ model_training.py
@@ -4,6 +4,7 @@ import load_data as ld
 import os
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+import pickle
 
 try:
 	df = pd.read_csv(os.getcwd() + "\\training_dataframe.csv")
@@ -28,5 +29,21 @@ def poly_fit(X, y, deg):
 	lm = LinearRegression().fit(Xpol, y)
 	return lm
 
+def save_model(model, model_name):
+	print()
+	print('Saving model ' + model_name + ' at: ', os.getcwd())
+	print()
+	print('Model path:', os.getcwd() + "\\" + model_name + ".sav")
+	print()
+	print()
+	pickle.dump(model, open(os.getcwd() + "\\" + model_name + ".sav", 'wb'))
 
+
+lin_reg = linear_fit(X, y)
+poly2fit = poly_fit(X, y, deg = 2)
+poly3fit = poly_fit(X, y, deg = 3)
+
+save_model(lin_reg, model_name = 'lin_reg')
+save_model(lin_reg, model_name = 'poly2fit')
+save_model(lin_reg, model_name = 'poly3fit')
 
